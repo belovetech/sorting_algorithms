29ae600e545c50380950d7980a5290d80842d906
Odunosh Moses <Odunoshomoses@gmail.com>
2022-07-16 20:09:14 +0000

    [fix]: added comment to swap function

--- swap.c
+++ swap.c
@@ -1,5 +1,12 @@
 #include "sort.h"
 
+/**
+ * swap - switch position of the elements passed
+ *
+ * @x: pointer the first element
+ * @x: pointer the second element
+ */
+
 void swap(int *x, int *y)
 {
     int temp;
