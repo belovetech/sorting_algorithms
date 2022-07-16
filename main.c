487ec70978973dbd40a06604256b9272e5a2ca41
belovetech <belovetech@gmail.com>
2022-07-16 20:59:22 +0100

    Add swap function

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+int main(void)
+{
+
+	size_t size = 5;
+	const int arr[5] = {10, 23, 4, 5, 7};
+
+	print_array(arr, 5);
+	return (0);
+}
---
+++ swap.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+void swap(int *x, int *y)
+{
+    int temp;
+
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
